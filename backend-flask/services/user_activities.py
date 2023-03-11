8625334e640afa468c8e13497a78ed8df7287951
coolchigi <coolchigi0031@gmail.com>
2023-03-11 01:38:12 +0000

    Disabled Xray, CW to avoid spend

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -3,7 +3,7 @@ from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
     ## X-Ray
-    segment = xray_recorder.begin_segment('user_activities')
+    #segment = xray_recorder.begin_segment('user_activities')
     model = {
       'errors': None,
       'data': None
@@ -23,10 +23,10 @@ class UserActivities:
       }]
       model['data'] = results
     ## X-Ray
-    dict = {
-      "now": now.isoformat(),
-      "results-size": len(model['data'])
-    }
-    subsegment = xray_recorder.begin_subsegment('mock-data')
-    subsegment.put_metadata('key', dict, 'namespace')
+    # dict = {
+    #   "now": now.isoformat(),
+    #   "results-size": len(model['data'])
+    # }
+    #subsegment = xray_recorder.begin_subsegment('mock-data')
+    #subsegment.put_metadata('key', dict, 'namespace')
     return model
