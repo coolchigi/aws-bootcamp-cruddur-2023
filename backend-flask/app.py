43ec39786d2085bd712e70132cb6f850c3123ca1
coolchigi <coolchigi0031@gmail.com>
2023-03-22 04:42:49 +0000

    Adde JWT token

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -91,8 +91,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -143,6 +143,8 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
+  app.logger.info("AUTH HEADER")
+  app.logger.info("AUTH HEADER")
   print("AUTH HEADER", file=sys.stdout)
   print(
     request.headers.get('Authorization')
